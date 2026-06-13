8995a87b9b21f24e3b8aacfdbd4ba5dd10469134
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-14 02:46:36 +0330

    feat: export Request and Response utils from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import huma from '@core/application';
 export { bodyParser } from '@middleware/bodyParser'
+export {Request} from '@core/request'
+export {Response} from '@core/response'
 export default huma;
